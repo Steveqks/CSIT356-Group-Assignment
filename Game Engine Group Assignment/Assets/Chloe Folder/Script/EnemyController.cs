89da579a07f96dfa5ee3c444cd3c03de35955b03
Chloe N <133016840+yier3an@users.noreply.github.com>
2024-08-18 21:34:42 +0800

    update spawner

--- Game Engine Group Assignment/Assets/Chloe Folder/Script/EnemyController.cs
+++ Game Engine Group Assignment/Assets/Chloe Folder/Script/EnemyController.cs
@@ -8,6 +8,7 @@ public class EnemyController : MonoBehaviour
 	private List<Transform> waypoints;
 	private int currentWP = 0;
 	[SerializeField] private float distanceToWP = 0.5f;
+	private bool WPset = false;
 	NavMeshAgent agent;
 
 	// Start is called before the first frame update
@@ -19,11 +20,28 @@ public class EnemyController : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		if (!WPset)
+		{
+			return;
+		}
 
+		if (!agent.pathPending && agent.remainingDistance <= distanceToWP)
+		{
+			if (currentWP == waypoints.Count - 1)
+			{
+				Destroy(this.gameObject);
+			}
+			else
+			{
+				currentWP++;
+				agent.SetDestination(waypoints[currentWP].position);
+			}
+		}
 	}
 
 	public void SetDestination(List<Transform> waypoints)
 	{
-		//
+		this.waypoints = waypoints;
+		WPset = true;
 	}
 }
--- Game Engine Group Assignment/Assets/Chloe Folder/Script/SimpleSpawner.cs
+++ Game Engine Group Assignment/Assets/Chloe Folder/Script/SimpleSpawner.cs
@@ -8,6 +8,8 @@ public class SimpleSpawner : MonoBehaviour
 {
 	public GameObject enemy_Prefab;
 	public List<Transform> waypoints;
+	public int maxEnemyOnField = 10;
+	private int enemyOnField;
 
 	// timer for spawner
 	public float startTimer = 1.0f;
@@ -15,13 +17,18 @@ public class SimpleSpawner : MonoBehaviour
 
 	void Start()
 	{
+		enemyOnField = 0;
 		InvokeRepeating(nameof(Spawn), startTimer, spawnTimer);
 	}
 
 	void Spawn()
 	{
-		GameObject enemy = Instantiate(enemy_Prefab, transform.position,
-			Quaternion.identity);
-		enemy.GetComponent<EnemyController>().SetDestination(waypoints);
+		if (enemyOnField <= maxEnemyOnField)
+		{
+			GameObject enemy = Instantiate(enemy_Prefab, transform.position,
+				Quaternion.identity);
+			enemy.GetComponent<EnemyController>().SetDestination(waypoints);
+			enemyOnField++;
+		}
 	}
 }
