c6aa2a50272d0854c3ea723ff1f66e9028a49224
Steve <148709785+Steveqks@users.noreply.github.com>
2024-08-19 20:33:29 +0800

    refine Player Status UI
    from initial title <blank> -> "empty"
    
    also refine PlayerStatus.cs script

--- Game Engine Group Assignment/Assets/Steve Folder/Scripts/PlayerStatus.cs
+++ Game Engine Group Assignment/Assets/Steve Folder/Scripts/PlayerStatus.cs
@@ -6,30 +6,33 @@ using UnityEngine;
 
 public class PlayerStatus : MonoBehaviour
 {
-    public int playerLives = 0;
-    public int playerMoney = 0;
+    [SerializeField] private int playerLives = 0;
+    [SerializeField] private int playerMoney = 0;
 
     // Variable to track the value that will increment every second
     private int incrementValue = 0;
 
     // Variable to keep track of time
     private float timeElapsed = 0f;
+    
+    // used for updating lives being displayed on player UI
+    private GameObject playerLivesTMP;
+    private TMP_Text textLives;
 
-    GameObject playerLivesTMP;
-    TMP_Text textLives;
-
-    GameObject playerMoneyTMP;
-    TMP_Text textMoney;
+    // used for updating money being displayed on player UI
+    private GameObject playerMoneyTMP;
+    private TMP_Text textMoney;
 
     // Start is called before the first frame update
     void Start()
     {
+        // retreive TMP text
         playerLivesTMP = GameObject.FindGameObjectWithTag("PlayerLives");
         playerMoneyTMP = GameObject.FindGameObjectWithTag("PlayerMoney");
 
+        // set TMP text to starting player status from PlayerStatus settings
         textLives = playerLivesTMP.GetComponent<TMP_Text>();
         textLives.text = playerLives.ToString();
-
         textMoney = playerMoneyTMP.GetComponent<TMP_Text>();
         textMoney.text = playerMoney.ToString();
     }
@@ -38,8 +41,8 @@ public class PlayerStatus : MonoBehaviour
     void Update()
     {
 
-        GameObject obj = GameObject.FindGameObjectWithTag("PlayerLives");
-        TMP_Text text = obj.GetComponent<TMP_Text>();
+        //GameObject obj = GameObject.FindGameObjectWithTag("PlayerLives");
+        //TMP_Text text = obj.GetComponent<TMP_Text>();
 
         // Add the time since the last frame to timeElapsed
         timeElapsed += Time.deltaTime;
@@ -47,9 +50,9 @@ public class PlayerStatus : MonoBehaviour
         // Check if one second has passed
         if (timeElapsed >= 1f)
         {
+            // for testing player live
             takeDamage(-1);
-            // set new PlayerMoney value
-            text.SetText(playerLives.ToString());
+
 
             // Increment the value
             //incrementValue += 1;
@@ -63,18 +66,22 @@ public class PlayerStatus : MonoBehaviour
             // Reset the timeElapsed, subtracting 1 to handle slight inaccuracies
             timeElapsed -= 1f;
         }
-        
-
-        
   
     }
-
-    public void towerSold(int value)
+    public int getPlayerLives()
     {
-        playerMoney += value;
+        return playerLives;
+    }
 
-        // set new PlayerMoney value
-        textMoney.SetText(playerMoney.ToString());
+    public int getPlayerMoney()
+    {
+        return playerMoney;
+    }
+    public void takeDamage(int value)
+    {
+        playerLives -= value;
+        textLives.text = playerLives.ToString();
+
     }
 
     public void towerBought(int value)
@@ -85,18 +92,12 @@ public class PlayerStatus : MonoBehaviour
         textMoney.SetText(playerMoney.ToString());
     }
 
-    public void takeDamage(int value)
+    public void towerSold(int value)
     {
-        playerLives -= value;
+        playerMoney += value;
+
+        // set new PlayerMoney value
+        textMoney.SetText(playerMoney.ToString());
     }
 
-    public int getPlayerLives()
-    {
-        return playerLives;
-    }
-
-    public int getPlayerMoney()
-    {
-        return playerMoney;
-    }
 }
